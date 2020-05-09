76d6d50a66b2bce9a0c670728364cae96060e58d
toyobayashi <lifenglin314@outlook.com>
2020-05-09 14:30:25 +0800

    default api-extractor config

--- command/dts.js
+++ command/dts.js
@@ -1,4 +1,4 @@
-const { runNpmBin, getPath } = require('../lib/util.js')
+const { getPath } = require('../lib/util.js')
 
 const { Task } = require('../lib/task.js')
 
@@ -9,7 +9,7 @@ module.exports = new Task('dts', async function (config/* , logger */) {
     info = dtsHack.applyChange(getPath('lib/esm'))
   }
   try {
-    await runNpmBin('api-extractor', ['run', '--local', '--verbose'])
+    invokeApiExtractor()
   } catch (err) {
     if (config.namespaceWrapper === true) {
       dtsHack.revertChange(info)
@@ -24,3 +24,44 @@ module.exports = new Task('dts', async function (config/* , logger */) {
   dtsHack.resolveDeclarationFile(dtsPath, config.library, format)
   return 0
 })
+
+function invokeApiExtractor () {
+  const fs = require('fs')
+  const path = require('path')
+  const {
+    Extractor,
+    ExtractorConfig
+  } = require('@microsoft/api-extractor')
+
+  const apiExtractorJsonPath = getPath('api-extractor.json')
+
+  let extractorConfig
+  if (fs.existsSync(apiExtractorJsonPath)) {
+    extractorConfig = ExtractorConfig.loadFileAndPrepare(apiExtractorJsonPath)
+  } else {
+    const configObjectFullPath = path.join(__dirname, '../tsconfig/api-extractor.json')
+    const packageJsonFullPath = getPath('package.json')
+    const configObject = ExtractorConfig.loadFile(configObjectFullPath)
+    // configObject.projectFolder = getPath()
+
+    extractorConfig = ExtractorConfig.prepare({
+      configObject,
+      configObjectFullPath: apiExtractorJsonPath, // fake path
+      packageJson: JSON.parse(fs.readFileSync(packageJsonFullPath, 'utf8')),
+      packageJsonFullPath: packageJsonFullPath
+    })
+  }
+
+  const extractorResult = Extractor.invoke(extractorConfig, {
+    // Equivalent to the "--local" command-line parameter
+    localBuild: true,
+
+    // Equivalent to the "--verbose" command-line parameter
+    showVerboseMessages: true
+  })
+
+  if (!extractorResult.succeeded) {
+    throw new Error(`API Extractor completed with ${extractorResult.errorCount} errors`
+      + ` and ${extractorResult.warningCount} warnings`)
+  }
+}
