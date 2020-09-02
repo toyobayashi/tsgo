9d75c4778f62d1212a8f0a4fe9916ba07b8b48d4
toyobayashi <lifenglin314@outlook.com>
2020-09-02 14:04:26 +0800

    0.2.1

--- command/gen.js
+++ command/gen.js
@@ -79,7 +79,7 @@ function getPackageJson ({ name, author }) {
       typescript: '3.9.7',
     },
     dependencies: {
-      '@tybys/native-require': '^1.3.1',
+      '@tybys/native-require': '^2.0.0',
       tslib: '2.0.1',
     }
   }, null, 2)
