07fddad32da4470f92d8e601fb2639a681776cfe
toyobayashi <lifenglin314@outlook.com>
2020-09-11 23:46:58 +0800

    0.2.2

--- command/doc.js
+++ command/doc.js
@@ -3,13 +3,15 @@ const { writeFileSync, readFileSync } = require('fs-extra')
 const { Task } = require('../lib/task.js')
 
 module.exports = new Task('doc', async function (config/* , logger */) {
-  const outputDir = getPath(config.output.doc || 'docs/api')
-  const { MarkdownAction } = require('@microsoft/api-documenter/lib/cli/MarkdownAction.js')
+  if (config.output.doc) {
+    const outputDir = getPath(config.output.doc || 'docs/api')
+    const { MarkdownAction } = require('@microsoft/api-documenter/lib/cli/MarkdownAction.js')
 
-  const markdownAction = new MarkdownAction()
-  markdownAction._inputFolderParameter = { value: getPath('temp') }
-  markdownAction._outputFolderParameter = { value: outputDir }
-  await markdownAction.onExecute()
-  writeFileSync(getPath(outputDir, 'README.md'), readFileSync(getPath(outputDir, 'index.md'), 'utf8'), 'utf8')
+    const markdownAction = new MarkdownAction()
+    markdownAction._inputFolderParameter = { value: getPath('temp') }
+    markdownAction._outputFolderParameter = { value: outputDir }
+    await markdownAction.onExecute()
+    writeFileSync(getPath(outputDir, 'README.md'), readFileSync(getPath(outputDir, 'index.md'), 'utf8'), 'utf8')
+  }
   return 0
 })
